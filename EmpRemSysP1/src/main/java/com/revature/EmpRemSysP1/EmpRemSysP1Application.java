1e4c63830a4d4f0fa0327ad7870d494543b82ac3
Chinedu <chineduezuma@yahoo.co.uk>
2024-12-19 22:12:32 -0600

    initial creation of Spring Boot Backend

---
+++ EmpRemSysP1/src/main/java/com/revature/EmpRemSysP1/EmpRemSysP1Application.java
@@ -0,0 +1,13 @@
+package com.revature.EmpRemSysP1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpRemSysP1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpRemSysP1Application.class, args);
+	}
+
+}
---
+++ EmpRemSysP1/src/test/java/com/revature/EmpRemSysP1/EmpRemSysP1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.revature.EmpRemSysP1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpRemSysP1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
